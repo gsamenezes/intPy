560d06d17525f6173398fb0444aba06a6cde0867
Guilherme Sá <gsa@inoa.com.br>
2022-09-24 21:17:37 -0300

    fix: eq solver args

--- eqsolver.py
+++ eqsolver.py
@@ -8,9 +8,7 @@ from intpy.intpy import initialize_intpy, deterministic
 
 T = 1
 C = 1
-F_solution = 0.5
 Kd1 = 2
-
 n2 = 1000
 Kd2 = 1
 
@@ -20,22 +18,21 @@ x_list = np.linspace(1, 2e4, n_solutions)
 F_list = np.empty_like(x_list)
 
 @deterministic
-def solve(n1):
-    return fsolve(func=lambda x: n1 * C / (1 + Kd1 / x) + n2 * C / (1 + Kd2 / x) - T, x0=F_solution, xtol=1e-6)
+def solve(n, F_solution):
+    return fsolve(func=lambda x: n * C / (1 + Kd1 / x) + n2 * C / (1 + Kd2 / x) - T, x0=F_solution, xtol=1e-6)
 
-
-if __name__ == '__main__':
-    n = int(sys.argv[1])
-    
-    start = time.perf_counter()
+@initialize_intpy(__file__)
+def main(F_solution):
     for i in range(n_solutions):
-        # Redefine n1
-        n1 = x_list[i]
-
         # Define function
-        F_solution = solve(n1, F_solution)
-        F_list[i] = F_solution
+        F_solution=solve(F_solution)
+        F_list[i]=F_solution
 
         # Print result
         print('x: %20.8f \t\tF: %20.8f' % (x_list[i], F_solution))
+
+if __name__ == '__main__':
+    F_solution = float(sys.argv[1])
+    start = time.perf_counter()
+    main(F_solution)
     print(time.perf_counter() - start)
